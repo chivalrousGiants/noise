b681d95821b237a68a66d16e84de05fdbb36507e
mofthecross <mofthecross@gmail.com>
2016-08-10 11:55:24 -0700

    Add client side socket source

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(request, response) {
   response.send('hello world');
@@ -7,4 +8,9 @@ app.get('/', function(request, response) {
 
 http.listen(4000, function() {
   console.log('listening')
+})
+
+io.on('connection', function(socket) {
+  console.log('a user connected')
+
 })
