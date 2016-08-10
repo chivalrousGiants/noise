c8408b8efa3da85d90078461d558dc485d283de3
mofthecross <mofthecross@gmail.com>
2016-08-10 12:16:28 -0700

    move socket source file path

---
+++ client/noise/socketio/SocketIOManager.swift
@@ -0,0 +1,13 @@
+//
+//  SocketIOManager.swift
+//  noise
+//
+//  Created by Michael DLC on 8/10/16.
+//  Copyright © 2016 Chivalrous Giants. All rights reserved.
+//
+
+import UIKit
+
+class SocketIOManager: NSObject {
+
+}
