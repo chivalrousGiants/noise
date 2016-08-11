f1730239365a5ef326bf5de024549138890a8db3
mofthecross <mofthecross@gmail.com>
2016-08-11 16:13:10 -0700

    Add alert function when user  register with empty fields

--- client/noise/signup/SignUpViewController.swift
+++ client/noise/signup/SignUpViewController.swift
@@ -1,15 +1,10 @@
-//
-//  SignUpViewController.swift
-//  noise
-//
-//  Created by Michael DLC on 8/9/16.
-//  Copyright © 2016 Chivalrous Giants. All rights reserved.
-//
 
 import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var userpasswordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +16,27 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func registerButtonTapped(sender: AnyObject) {
+        let userName = usernameTextField.text
+        let userPassword = userpasswordTextField.text
+        
+        if(userName!.isEmpty || userPassword!.isEmpty){
+            displayAlertMessage("All fields are required!")
+            return
+        }
+
+    }
+    
+    func displayAlertMessage(userMessage: String)
+    {
+        let myAlert = UIAlertController(title:"Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        
+        myAlert.addAction(okAction)
+        self.presentViewController(myAlert, animated: true, completion: nil)
+        
+    }
 
     /*
     // MARK: - Navigation
