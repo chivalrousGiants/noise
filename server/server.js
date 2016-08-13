f66d1392702189716fb459a73db9d9a8a3f79cf9
Hannah_Brannan <hannahbrannan@gmail.com>
2016-08-13 06:56:01 -0700

    Add mods

--- server/server.js
+++ server/server.js
@@ -18,21 +18,34 @@ io.on('connection', function(clientSocket) {
 
   clientSocket.on('disconnect', function() {
   console.log('user disconnected')
-  })
+  });
+
+  clientSocket.on('userSignedUp', function() {
+  console.log('user signed up')
+  });
+
+  clientSocket.on('userLoggedIn', function() {
+  console.log('user logged in')
+  });
 
   clientSocket.on('chatSent', function(chatMessage) {
-  	console.log('TODO: pass encrypted chatMessage to redis DB')
-  	//clientSocket.emit('c', chatMessage);
-  })
+  	console.log('STRETCH: pass chatMessage: encrypted to redis DB')
+  	//insert into redis
+  });
+
+  clientSocket.on('noisifiedChatSent', function(chatMessage) {
+	console.log('TODO: pass nosified chatMessage to redis DB')
+	//clientSocket.emit('c', chatMessage);
+  });
 
   clientSocket.on('getfriends', function() {
     var dummyFriends = ["Ryan", "Jae", "Michael", "Hannah"]
     io.emit(dummyFriends);
 
-  })
+  });
 
   clientSocket.on('signinOrSignup', function(username) {
     users.push(username)
     console.log(username, 'connected and userscount is', users.length, username )
-  })
-})
+  });
+});
