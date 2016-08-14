56ea14efa2e39a477a28d060db1a7b0b86dbddfa
mofthecross <mofthecross@gmail.com>
2016-08-13 21:21:09 -0700

    create new class and insert to realm

--- client/noise/friendsList/AddFriendViewController.swift
+++ client/noise/friendsList/AddFriendViewController.swift
@@ -1,39 +1,32 @@
-//
-//  AddFriendViewController.swift
-//  noise
-//
-//  Created by Michael DLC on 8/9/16.
-//  Copyright © 2016 Chivalrous Giants. All rights reserved.
-//
-
 import UIKit
+import RealmSwift
+
 
 class AddFriendViewController: UIViewController {
 
+    @IBOutlet weak var addFriendTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
+      
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func addFriendTapped(sender: AnyObject) {
+        let newFriend = User()
+        newFriend.username = "MDLC"
+        newFriend.lastname = "DeLaCruz"
+        newFriend.firstname = "MDLC"
+        
+        let realm = try! Realm()
+        try! realm.write{
+            realm.add(newFriend)
+        }
+        let friends = realm.objects(User)
+        print(friends)
     
-    @IBAction func confirmAddFriendClicked(sender: AnyObject) {
-        let newFriend = "whateverFriendYouSelected"
-        SocketIOManager.sharedInstance.addFriend(newFriend)
-    }
+   }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ client/noise/friendsList/FriendModel.swift
@@ -0,0 +1,17 @@
+//
+//  FriendModel.swift
+//  noise
+//
+//  Created by Michael DLC on 8/13/16.
+//  Copyright © 2016 Chivalrous Giants. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+
+class User : Object {
+    dynamic var username = ""
+    dynamic var lastname = ""
+    dynamic var firstname = ""
+}
