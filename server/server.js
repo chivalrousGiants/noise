f1593d7b34865312bbf1a71a5a60f4b0a8fb5aa4
mofthecross <mofthecross@gmail.com>
2016-08-14 23:40:16 -0700

    merge from upstream w/ Hannah's pr

--- server/server.js
+++ server/server.js
@@ -12,22 +12,21 @@ app.get('/', function(request, response) {
 });
 
 http.listen(4000, function() {
-  console.log('Server listening at port 4000');
+  console.log('listening')
 });
 
-// forDummyData
-//let users = [];
+//forDummyData
+// let users = [];
 
 io.on('connection', function(clientSocket) {
   console.log('A user connected with socket id', clientSocket.id);
-
 
   clientSocket.on('disconnect', function() {
     console.log('A user disconnected with socket id', clientSocket.id);
   });
 
-
   clientSocket.on('signIn', function(user) {
+
     console.log('hit signIn on server socket:', user);
     console.log('typeof user:', typeof user);
 
@@ -36,12 +35,11 @@ io.on('connection', function(clientSocket) {
     // check utils.signIn(user)
     // communicate false or true back to front-end
     //
-    userController.signIn(user, clientSocket);
+    userController.signIn(user, clientSocket.id);
   });
 
-  clientSocket.on('userSigningUp', function(user) {
-    console.log('hit signUp on server socket: ', user);
-    userController.signUp(user, clientSocket);
+  clientSocket.on('userSigningIn', function() {
+  console.log('user logged in')
   });
 
   clientSocket.on('encryptedChatSent', function(chatMessage) {
