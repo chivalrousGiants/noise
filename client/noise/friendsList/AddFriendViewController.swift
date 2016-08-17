cb064ce9e3df73af86886dd77208f139ef7fabdd
mofthecross <mofthecross@gmail.com>
2016-08-17 15:45:43 -0700

    merge pull request

--- client/noise/friendsList/AddFriendViewController.swift
+++ client/noise/friendsList/AddFriendViewController.swift
@@ -19,25 +19,47 @@ class AddFriendViewController: UIViewController {
             object: nil)
 
         let friendToAdd = addFriendTextField.text
-        // for testing, will be replaced with a real socket response
-        let respondFromSocket = false
-        // send friendToAdd to server via socket
-            //if friendToAdd is found in redisdb
-            if respondFromSocket {
-                let newFriend = Friend(value: [
-                    "username" : friendToAdd!,
-                    "firstname" : "dummy",
-                    "lastname" : "dummy",
-                    "photo" : "optional",
-                    "friendID": "userID"
-                    ])
-                try! realm.write{
-                    realm.add(newFriend)
-                }
-                performSegueWithIdentifier("backToFriendsListSegue", sender: self)
-            } else {
-                 displayAlertMessage("username not found!")
+        
+        // SEND: friendToAdd to server
+        SocketIOManager.sharedInstance.addFriend(friendToAdd!)
+    }
+    
+    @objc func handleAddFriendNotification(notification: NSNotification) -> Void {
+        
+        print("friendObj", notification.userInfo)
+        
+        if let userObj = notification.userInfo {
+            
+            // insert new friend data in realm
+            let newFriend = Friend()
+            newFriend.firstname = userObj["firstname"] as! String
+            newFriend.lastname = userObj["lastname"] as! String
+            newFriend.username = userObj["username"] as! String
+            
+            try! realm.write {
+                realm.add(newFriend)
+            }
+            
+            performSegueWithIdentifier("backToFriendsListSegue", sender: self)
+        } else {
+            // friendToAdd was NOT found in redis db
+            let alert:UIAlertController = UIAlertController(title: "Ooftah!", message: "no friend of that username exists", preferredStyle: UIAlertControllerStyle.Alert)
+            let action:UIAlertAction = UIAlertAction(title: "bummer", style: UIAlertActionStyle.Default) { (a: UIAlertAction) -> Void in
+                print("bummer button selected")
+            }
+            alert.addAction(action)
+            
+            self.presentViewController(alert, animated:true) { () -> Void in
+                print("alert presented for unsuccessful addNewFriend")
+            }
         }
+        
+        // Remove listener
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+        
+        // Testing
+        print("Friends list:", realm.objects(Friend))
+        
    }
     
     func displayAlertMessage(message: String) {
