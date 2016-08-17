82c81859e579809d6f636163c9221746a6ce0b22
mofthecross <mofthecross@gmail.com>
2016-08-17 12:56:37 -0700

    implement chat bubble for messages, add collection view controller and cell

---
+++ client/noise/chatscreen/ChatCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ChatCollectionViewCell.swift
+//  noise
+//
+//  Created by Michael DLC on 8/17/16.
+//  Copyright © 2016 Chivalrous Giants. All rights reserved.
+//
+
+import UIKit
+
+class ChatCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var receiveChatLabel: UILabel!
+}
---
+++ client/noise/chatscreen/ChatCollectionViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ChatViewController.swift
+//  noise
+//
+//  Created by Michael DLC on 8/17/16.
+//  Copyright © 2016 Chivalrous Giants. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var CollectionView: UICollectionView!
+    
+    let messages = ["hello", "hi"]
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("herreeeee")
+        
+        self.CollectionView.dataSource = self
+        self.CollectionView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        print("where?")
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ChatCell",
+            forIndexPath: indexPath) as! ChatCollectionViewCell
+        
+        cell.receiveChatLabel.layer.cornerRadius = 5
+        cell.receiveChatLabel.layer.masksToBounds = true
+        
+        cell.receiveChatLabel.text = "HEY sdfdsgfdsgsfdgtgfdgdsrtsert the ovdsgsvherewafdcrertfsdcvfgvcxfdgvsdzsgxfdgsvzxvcd"
+        
+        return cell
+    }
+ 
+
+}
