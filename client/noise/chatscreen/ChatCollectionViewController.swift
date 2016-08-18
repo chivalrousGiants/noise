654dc7145e3400830aaca12a9cc1c61c7cd1cb47
mofthecross <mofthecross@gmail.com>
2016-08-17 20:31:08 -0700

    write to realm database when use taps send button

--- client/noise/chatscreen/ChatCollectionViewController.swift
+++ client/noise/chatscreen/ChatCollectionViewController.swift
@@ -1,19 +1,25 @@
 import UIKit
+import RealmSwift
 
-class ChatViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ChatViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UITextFieldDelegate {
     @IBOutlet weak var CollectionView: UICollectionView!
     @IBOutlet weak var SendChatTextField: UITextField!
     @IBOutlet weak var NavigationLabel: UINavigationItem!
+    @IBOutlet weak var MessageTextFieldLabel: UITextField!
+    
+    let realm = try! Realm()
+    var friend = Friend()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(friend)
         self.CollectionView.dataSource = self
         self.CollectionView.delegate = self
-        self.NavigationLabel.title = "Michael"
+        self.NavigationLabel.title =  friend.firstname
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        return 50
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -30,11 +36,20 @@ class ChatViewController: UIViewController, UICollectionViewDataSource, UICollec
         // cell.sendChatLabel.layer.masksToBounds = true
         
         // sample chat
-        cell.receiveChatLabel.text = "The Quick Brown Fox Jumps Over the Lazy Dog"
+        cell.receiveChatLabel.text = "YOO0000000000 WHATS GOING ON"
         return cell
     }
  
     @IBAction func sendButtonTapped(sender: AnyObject) {
+        let message = Message()
+        message.text = self.MessageTextFieldLabel.text!
+        message.receiver = "jae"
         
+        try! realm.write {
+            realm.add(message)
+            let sent = realm.objects(Message)
+            print(sent)
+        }
     }
-}
+    
+  }
