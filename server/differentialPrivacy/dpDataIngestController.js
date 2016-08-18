4b1458ebd374f558530379f589cb27566ca3737b
rhanzawa <rhanzawa@gmail.com>
2016-08-17 17:16:56 -0700

    Remove development comments from dpDataIngestController

--- server/differentialPrivacy/dpDataIngestController.js
+++ server/differentialPrivacy/dpDataIngestController.js
@@ -45,48 +45,6 @@ function IngestIRRReports(IRRReports) {
   });
 }
 
-// Testing
-// for (let i = 0; i < 1000; i++) {
-//   IngestIRRReports(SampleIRRReports);
-// }
-
-// function signUp (user, clientSocket) {
-//   redis.client.hgetAsync('users', user.username)
-//     .then(userId => {
-//       console.log('signUp userId is', userId);
-
-//       if (userId === null) {
-//         // Username not taken, thus valid for new user
-        
-//         // Initiate insertion of new user by incrementing global_userId key
-//         redis.client.incr('global_userId', redis.print);
-
-//         return redis.client.getAsync('global_userId');
-
-//       } else {
-//         // Username already exists
-//         return null;
-//       }
-//     })
-//     .then(globalUserId => {
-
-//       // Username already exists
-//       if (globalUserId === null) {
-
-//         clientSocket.emit('redis response for signup', null);
-
-//       } else {
-
-//         redis.client.hmset(`user:${globalUserId}`, ['firstname', user.firstname, 'lastname', user.lastname, 'username', user.username, 'password', user.password], function(err, res) {});
-//         redis.client.hset('users', [user.username, `${globalUserId}`]);
-        
-//         clientSocket.emit('redis response for signup', user);
-
-//       }
-//     })
-//     .catch(console.error.bind(console));
-// }
-
 // Exports
 module.exports = {
   IngestIRRReports,
