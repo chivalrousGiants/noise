033f27485833599ac088d1aba26a428ed9f42bcf
Hannah_Brannan <hannahbrannan@gmail.com>
2016-08-17 17:30:31 -0700

    Add Utils extension (currently set on number). On friendSelect, compute a-e for alice. Layout plans in comments.

--- client/noise/friendsList/FriendsListViewController.swift
+++ client/noise/friendsList/FriendsListViewController.swift
@@ -34,6 +34,33 @@ class FriendsListViewController: UIViewController, UITableViewDataSource, UITabl
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //////////CODE INSIDE OF COMMENT -> POPUP INIT WHEN READY
+        
+        //fetch username from the selected cell
+        let user = self.friends![indexPath.row]
+        let username = user.username
+        print("Username is \(username)")
+        //get Alice's userId from redis
+        //get Bob's userId from redis
+        //query if user1/user2 has a chat history
+           //YES >>> segue
+           //NO >>>
+                //compute DHX numbers
+                let g_Alice = 666.gCreate()
+                    print("her g is \(g_Alice)")
+                let p_Alice = 666.pCreate()
+                    print("her p is \(p_Alice)")
+                let a_Alice = 666.aAliceCreate()
+                    print("her a is \(a_Alice)")
+                let E_Alice = 666.eCreate(g_Alice, mySecret: a_Alice, p: p_Alice)
+                    print("her e is \(E_Alice)")
+                //insert a_Alice into keychain
+                //insert g_Alice, p_Alice, E_Alice into the Redis DB
+                //insert Alice into Bob's pending.
+                      ///???????????? insert Bob into Alice's pending. 
+        
+        ////////////////////////////////////////////////////////////////
+        
         self.performSegueWithIdentifier("chatScreenSegue", sender: self)
     }
     
