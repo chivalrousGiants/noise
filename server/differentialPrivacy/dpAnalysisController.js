65a43a72a444012be4aab7ecf5fd6d9bce043c6e
rhanzawa <rhanzawa@gmail.com>
2016-08-22 14:54:22 -0700

    Add helper functions in dpAnalysisController

--- server/differentialPrivacy/dpAnalysisController.js
+++ server/differentialPrivacy/dpAnalysisController.js
@@ -21,6 +21,19 @@ const sampleCandidateStrings = ['v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 
   'v171', 'v172', 'v173', 'v174', 'v175', 'v176', 'v177', 'v178', 'v179', 'v180', 'v181', 'v182', 'v183', 'v184', 'v185', 'v186', 'v187', 'v188', 'v189', 'v190',
   'v191', 'v192', 'v193', 'v194', 'v195', 'v196', 'v197', 'v198', 'v199', 'v200'];
 
+// Generate the counts file containing the total number of reports and bit sums
+// for each cohort.
+// Returns a Promise that is resolved with the string contents of the counts file.
+function generateCountsString() {
+
+}
+
+// Given an Array of candidate strings, generate the map file containing
+// each candidate string and the bits that it maps to in each cohort.
+function generateMapFile(candidateStrings) {
+  
+}
+
 // Given an Array of candidate strings, using aggregated data in Redis,
 // returns a Promise that is resolved with an Array of detected strings.
 function performDPAnalysis() {}
