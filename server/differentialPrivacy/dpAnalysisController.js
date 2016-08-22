f4664a41fa67094b5ac8719c6a8bbcf19b58ec0c
rhanzawa <rhanzawa@gmail.com>
2016-08-22 15:54:11 -0700

    Add OpenCPU POST request options object

--- server/differentialPrivacy/dpAnalysisController.js
+++ server/differentialPrivacy/dpAnalysisController.js
@@ -15,6 +15,18 @@ const {
   MAX_SUM_BITS,
 } = require('./dpParams');
 
+// Options for POST to OpenCPU server
+const postOptions = {
+  host: 'localhost',
+  path: '/ocpu/library/rappor/R/Decode',
+  port: 8004,
+  timeout: 120000,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+};
+
 const sampleCandidateStrings = ['v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 'v9', 'v10', 'v11', 'v12', 'v13', 'v14', 'v15', 'v16', 'v17', 'v18', 'v19', 'v20',
   'v21', 'v22', 'v23', 'v24', 'v25', 'v26', 'v27', 'v28', 'v29', 'v30', 'v31', 'v32', 'v33', 'v34', 'v35', 'v36', 'v37', 'v38', 'v39', 'v40', 'v41', 'v42', 'v43',
   'v44', 'v45', 'v46', 'v47', 'v48', 'v49', 'v50', 'v51', 'v52', 'v53', 'v54', 'v55', 'v56', 'v57', 'v58', 'v59', 'v60', 'v61', 'v62', 'v63', 'v64', 'v65', 'v66',
@@ -93,7 +105,7 @@ function generateParamsFile() {
 // Given an Array of candidate strings, using aggregated data in Redis,
 // returns a Promise that is resolved with an Array of detected strings.
 function performDPAnalysis(candidateStrings) {
-
+  generateCountsString
 }
 
 // Exports
