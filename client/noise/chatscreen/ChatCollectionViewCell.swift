3dc73793755ffa50946255a88f66bb3e432b5191
mofthecross <mofthecross@gmail.com>
2016-08-25 02:19:49 -0700

    delete chatViewCell file since int's no longer used

--- client/noise/chatscreen/ChatCollectionViewCell.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-//reusable cell
-class ChatCollectionViewCell: UICollectionViewCell {
-    
-    @IBOutlet weak var receiveChatLabel: UILabel!
-    @IBOutlet weak var sendChatLabel: UILabel!
-}
